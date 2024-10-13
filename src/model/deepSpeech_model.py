b7aa65114366a6b42dc2bdf53139dfff7cb682fd
wrknatul <wrk.natul@yandex.ru>
2024-10-13 15:59:53 +0300

    fixing deepspeech

--- src/model/deepSpeech_model.py
+++ src/model/deepSpeech_model.py
@@ -12,23 +12,24 @@ class RNNLayer(nn.Module):
     def __init__(
         self,
         input_size: int,
-        hidden_size: int,
+        rnn_hidden_size: int,
         bidirectional: bool = False,
+        **batch
     ):
         super(RNNLayer, self).__init__()
         self.input_size = input_size
-        self.hidden_size = hidden_size
+        self.hidden_size = rnn_hidden_size
         self.bidirectional = bidirectional
 
         self.num_directions = 2 if bidirectional else 1
         self.rnn = nn.LSTM(
             input_size,
-            hidden_size,
+            rnn_hidden_size,
             bidirectional=bidirectional,
             batch_first=True,
         )
         if bidirectional:
-            self.down_projection = nn.Linear(hidden_size * 2, hidden_size)
+            self.down_projection = nn.Linear(rnn_hidden_size * 2, rnn_hidden_size)
 
     def forward(self, x, lengths):
         batch_size = x.size(0)
@@ -41,33 +42,43 @@ class RNNLayer(nn.Module):
 
 
 class DeepSpeechModel(BaselineModel):
-    def __init__(self, input_channels, n_tokens,
-        rnn_params = {"num_rnn": 5,  "rnn_hid": 512, "bi": True},
+    # def __init__(self, input_channels, n_tokens, **batch):
+    def __init__(self, input_channels, n_tokens, **batch):
+
+        rnn_params = {"num_rnn": 5, "rnn_hidden_size": 512, "bidirectional": True}
         conv_params = [
-  {
-    "in_channels": 1,
-    "out_channels": 32,
-    "kernel_size": [41, 11],
-    "stride": [2, 2]
-  },
-  {
-    "in_channels": 32,
-    "out_channels": 32,
-    "kernel_size": [21, 11],
-    "stride": [2, 1]
-  }
-], **batch):
+            {
+                "in_channels": 1,
+                "out_channels": 32,
+                "kernel_size": [41, 11],
+                "stride": [2, 2]
+            },
+            {
+                "in_channels": 32,
+                "out_channels": 32,
+                "kernel_size": [21, 11],
+                "stride": [2, 1]
+            }
+        ]
         super().__init__(input_channels, n_tokens, **batch)
+
         self.input_channels = input_channels
         self.n_tokens = n_tokens
         self.conv_params = conv_params
 
         convs = []
         rnn_feat = input_channels
-        for out_channels, kernel_size in self.conv_params:
+        for layer_ in self.conv_params:
+            (out_channels, kernel_size) = layer_["out_channels"],  layer_["kernel_size"] 
+            in_channels = layer_["in_channels"]
+            stride = layer_['stride']
+            rnn_feat = rnn_feat // stride[0]
             convs.append(
                 nn.Sequential(
-                    nn.Conv2d(out_channels=out_channels, kernel_size=kernel_size, padding=tuple(np.array(kernel_size) // 2), bias=False),
+                    nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                        padding=tuple(np.array(kernel_size) // 2),
+                        stride=tuple(stride),
+                          bias=False),
                     nn.BatchNorm2d(num_features=out_channels),
                     nn.Hardtanh(0, 20)
                 )
